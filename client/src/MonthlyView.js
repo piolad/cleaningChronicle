305e1fb841e7f4d8e1a58f0752624b7a87554e69
piolad <piolad2003@gmail.com>
2023-10-18 11:46:37 +0200

    move tasks to list of components

--- client/src/MonthlyView.js
+++ client/src/MonthlyView.js
@@ -3,6 +3,20 @@ import './MonthlyView.css'
 
 function MonthlyView(){
 
+    var kitchenTasks = [ 
+        <TaskElement cleaner={"Mokrzyk"} task={"Mycie mikrofalówki"}></TaskElement>,
+        <TaskElement cleaner={"Paweł"} task={"Zjedzenie ziemniaka"}></TaskElement>
+    ]
+
+    var bathroomTasks = [
+        <TaskElement cleaner={"Piotrek"} task={"Umycie dupska"}></TaskElement>
+    ]
+
+    var wcTasks = []
+    var corridorTasks = []
+    var livingroomTasks = []
+    var otherTasks = []
+    
     return(
     <div className='view'>
           {/* example view */}
@@ -10,24 +24,27 @@ function MonthlyView(){
           <div class="container">
             <div class="kitchen">
               <h3>Kuchnia</h3>
-              <TaskElement cleaner={"Mokrzyk"} task={"Mycie mikrofalówki"}></TaskElement>
-              <TaskElement cleaner={"Paweł"} task={"Zjedzenie ziemniaka"}></TaskElement>
+              {kitchenTasks}
             </div>
             <div class="bathroom">
               <h3>Łazienka</h3>
-              <TaskElement cleaner={"Piotrek"} task={"Umycie dupska"}></TaskElement>
+              {bathroomTasks}
             </div>
             <div class="wc">
               <h3>Kibelson</h3>
+              {wcTasks}
             </div>
             <div class="corridor">
               <h3>Korytatz</h3>
+              {corridorTasks}
             </div>
             <div class="livingroom">
               <h3>Salon</h3>
+              {livingroomTasks}
             </div>
             <div class="other">
               <h3>Inne</h3>
+              {otherTasks}
             </div>
           </div>
     </div>
