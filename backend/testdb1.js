288b3136b3e0867dc65c1b6701355c166b4d9a2c
piolad <piolad2003@gmail.com>
2023-11-04 18:59:18 +0100

    modified:   backend/testdb1.js

--- backend/testdb1.js
+++ backend/testdb1.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 // Route for the default page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 // Route for getting data from the database
